7bb0345b3491a8575447841627107f4264a69f8a
kimjungho <kwjdgh1267@gmail.com>
2025-04-10 10:17:14 +0900

    Fix: FriendService approveFriend 수정
    없는 사용자를 파라미터로 주거나 친구 요청이 없을 때에도 친구 수락으로
    표시되던 오류 수정

--- src/main/java/i_talktalk/i_talktalk/service/FriendService.java
+++ src/main/java/i_talktalk/i_talktalk/service/FriendService.java
@@ -76,12 +76,12 @@ public class FriendService {
         Member currentMember = memberRepository.findById(userDetails.getUsername()).get();
         Optional<Member> requestedMember = memberRepository.findByName(name);
         if(!requestedMember.isPresent()){
-            return "해당 유저는 친구 요청을 보내지 않았습니다.";
+            throw new MemberNotFoundException("없는 사용자 입니다.");
         }
 
         Optional<Friend> foundFriend = friendRepository.findByMember2AndMember1(currentMember, requestedMember.get());
         if(!foundFriend.isPresent()){
-            return "친구 요청이 없습니다.";
+            throw new MemberNotFoundException("친구 요청이 없습니다.");
         }
         foundFriend.get().setApproved(true);
         return "친구 수락 완료!";
