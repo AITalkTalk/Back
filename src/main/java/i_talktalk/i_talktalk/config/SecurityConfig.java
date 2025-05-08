fb31b2fa9abc236b1d6eb96a622e8cdb782bad61
kimjungho <kwjdgh1267@gmail.com>
2025-05-08 12:25:58 +0900

    Feat: docker-compose.yml changed

--- src/main/java/i_talktalk/i_talktalk/config/SecurityConfig.java
+++ src/main/java/i_talktalk/i_talktalk/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
         return httpSecurity.httpBasic((hp)->hp.disable())
                 // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
                 .csrf((cs)->cs.disable())
+                .httpBasic((hp) -> hp.disable())  // 기본 인증 비활성화
                 // JWT를 사용하기 때문에 세션을 사용하지 않음
                 .sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((ahr)->ahr.requestMatchers(
